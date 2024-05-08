4245c56d49b01011df514934feafb4b42df5e45d
botmi2510 <kudotruong2002@gmail.com>
2024-05-09 05:40:43 +0700

    #20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,8 +9,13 @@ class MyComponent extends React.Component {
         address: '147 Yen Hoa',
         age: 21
     };
-    handleClick(event) {
-        console.log('my name is ', this.state.name);
+    handleClick = (event) => {
+        // console.log('my name is ', this.state.name);
+        // console.log(`i'm `, this.state.age);
+        this.setState({
+            name: 'Pham Xuan Truong',
+            age: Math.floor((Math.random() * 100) + 1),
+        })
     }
     //jsx
     render() {
@@ -18,7 +23,7 @@ class MyComponent extends React.Component {
             <div>
                 My name is {this.state.name}
                 My address is {this.state.address} and my age is {this.state.age} years old
-                <button onClick={this.handleClick}>Click</button>
+                <button onClick={(event) => { this.handleClick() }}>Click</button>
             </div>
         );
     }
