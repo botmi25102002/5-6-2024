05e338c4367a3705803c4d958ec9f352e4a1be39
botmi2510 <kudotruong2002@gmail.com>
2024-05-20 01:30:11 +0700

    #51

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,8 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">PXT</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">PXT</Navbar.Brand> */}
+                <NavLink to="/" className=" navbar-brand">PXT</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -22,11 +23,9 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Log Out
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Log in</NavDropdown.Item>
+                            <NavDropdown.Item >Log Out</NavDropdown.Item>
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
                             <NavDropdown.Divider />
                         </NavDropdown>
                     </Nav>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,8 @@
+const HomePage = () => {
+    return (
+        <div className="homepage-container">
+            This is the HomePage
+        </div>
+    )
+}
+export default HomePage;
